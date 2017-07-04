55b9580c4ba8922c86b7e6ede718056e90bd91ae
bolu ajibs <bolu.ajibawo@gmail.com>
2017-07-04 13:43:13 +0100

    changed port

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 var parser = require('ua-parser-js');
 
 
+app.set('port', (process.env.PORT || 5000));
+
 // in order to detect proxies and correctly register client's ip address
 app.enable('trust proxy');
 
@@ -13,7 +15,7 @@ app.enable('trust proxy');
 // Home Page
 app.get('/', function (req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Visit localhost:5000/whoami to get the ' +
+	res.end('Visit https://ajibs-request-header-parser.herokuapp.com/whoami to get the ' +
 		'IP address, language and operating system for your browser.');
 });
 
@@ -33,5 +35,6 @@ app.get('/whoami', function (req, res){
 	res.end(JSON.stringify(result));
 })
 
-app.listen(5000);
-console.log('Node app is running on port 5000');
+app.listen(app.get('port'), function(){
+		console.log('Node app is running on port', app.get('port'));
+});
